497dbc66948db3f77a8935d496a75ad810cd3598
samian2002 <samiantariq14@gmail.com>
2025-02-12 12:31:58 +0100

    tipi primitivi

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,12 +1,16 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-
+const name = "Samian"
+console.log(name)
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+const number = 20;
+const text = 'Ciao';
+const bool = true;
+console.log(`Numero: ${number}, Testo: ${text}, Booleano: ${bool}`);
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
